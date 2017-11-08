29e73cc4d804cd6400b0a6951eaccfd2e0e32e35
haibin <3525004748@qq.com>
2017-11-08 15:33:30 +0800

    test: enhanced test cases

--- tests/test_core.py
+++ tests/test_core.py
@@ -6,7 +6,7 @@ except ImportError:
     from mock import MagicMock
 
 from pysm.core import Machine, State, Event, state_machine
-from pysm.error import AlreadyHasInitialState, InvalidTransition
+from pysm import error
 
 
 @state_machine('test', Machine)
@@ -31,10 +31,62 @@ class TestCore(TestCase):
         self.assertIsNotNone(m.initial)
         self.assertEqual(m.initial, 'A')
 
-        with self.assertRaises(AlreadyHasInitialState):
+        with self.assertRaises(error.AlreadyHasInitialState):
             m.set_initial_state('C')
         m.set_initial_state('C', force=True)
         self.assertEqual(m.initial, 'C')
+
+        with self.assertRaises(error.NoState):
+            m.set_initial_state('X', force=True)
+
+    def test_add_state(self):
+        m = Stuff.machine
+        self.assertDictEqual(m.states, {})
+
+        states = ['A', 'B', {'name': 'C'}, 'D']
+        m.add_states(states)
+        self.assertTrue(len(m.states) == 4)
+
+        with self.assertRaises(error.AlreadyHasState):
+            m.add_state('A')
+
+    def test_get_state(self):
+        states = ['A', 'B', {'name': 'C'}, 'D']
+        m = Stuff.machine
+        m.add_states(states, initial='A', force=True)
+        self.assertIsNotNone(m.get_state('A'))
+
+        with self.assertRaises(error.NoState):
+            m.get_state('X')
+
+    def test_add_transition(self):
+        states = ['A', 'B', {'name': 'C'}, 'D']
+        # Define with list of dictionaries
+        transitions = [
+            {'event': 'walk', 'from_state': 'A', 'to_state': 'B'},
+            {'event': 'run', 'from_state': 'B', 'to_state': 'C'},
+            {'event': 'sprint', 'from_state': 'C', 'to_state': 'D'},
+            {'event': 'run', 'from_state': 'C', 'to_state': 'A'},
+        ]
+        m = Stuff.machine
+        m.add_states(states, initial='A', force=True)
+        m.add_transitions(transitions)
+        self.assertEqual(len(m.transitions), 4)
+
+        # Define with list of lists
+        transitions = [
+            ['A', 'B', 'walk'],
+            ['B', 'C', 'run'],
+            ['C', 'D', 'sprint']
+        ]
+        m._reset()
+        m.add_states(states, initial='A')
+        m.add_transitions(transitions)
+        with self.assertRaises(error.NoState):
+            m.add_transition('X', 'B', 'walk')
+
+        with self.assertRaises(error.NoState):
+            m.add_transition('A', 'X', 'walk')
 
     def test_dispatch(self):
         states = [State('State1'), 'State2', {'name': 'State3'}]
@@ -53,43 +105,6 @@ class TestCore(TestCase):
         s.dispatch(Event('advance'))
         self.assertEqual(s.state, 'State3')
 
-    def test_transition_definitions(self):
-        states = ['A', 'B', {'name': 'C'}, 'D']
-        # Define with list of dictionaries
-        transitions = [
-            {'event': 'walk', 'from_state': 'A', 'to_state': 'B'},
-            {'event': 'run', 'from_state': 'B', 'to_state': 'C'},
-            {'event': 'sprint', 'from_state': 'C', 'to_state': 'D'},
-            {'event': 'run', 'from_state': 'C', 'to_state': 'A'},
-        ]
-        m = Stuff.machine
-        m.add_states(states, initial='A', force=True)
-        m.add_transitions(transitions)
-
-        s = Stuff()
-        s.dispatch(Event('walk'))
-        self.assertEqual(s.state, 'B')
-        s.dispatch(Event('run'))
-        self.assertEqual(s.state, 'C')
-        s.dispatch(Event('run'))
-        self.assertEqual(s.state, 'A')
-
-        # Define with list of lists
-        transitions = [
-            ['A', 'B', 'walk'],
-            ['B', 'C', 'run'],
-            ['C', 'D', 'sprint']
-        ]
-        m._reset()
-        m.add_states(states, initial='A')
-        m.add_transitions(transitions)
-
-        s = Stuff()
-        s.dispatch(Event('walk'))
-        s.dispatch(Event('run'))
-        s.dispatch(Event('sprint'))
-        self.assertEqual(s.state, 'D')
-
     # def test_add_custom_state(self):
     #     s = self.stuff
     #     s.machine.add_states([{'name': 'E', 'children': ['1', '2', '3']}])
@@ -98,7 +113,7 @@ class TestCore(TestCase):
     #     s.go()
     #     s.run()
 
-    def test_enter_exit_nested_state(self):
+    def test_enter_exit_state(self):
         mock = MagicMock()
 
         def callback(state, event):
@@ -137,7 +152,23 @@ class TestCore(TestCase):
         s.dispatch(Event('stop'))
         s.dispatch(Event('drink'))
         self.assertEqual(s.state, 'caffeinated')
-        with self.assertRaises(InvalidTransition):
+        with self.assertRaises(error.InvalidTransition):
             s.dispatch(Event('stop'))
         s.dispatch(Event('relax'))
         self.assertEqual(s.state, 'standing')
+
+    def test_switch_state(self):
+        # private api
+        states = ['A', 'B', 'C', 'D']
+        transitions = [['A', 'C', 'go'], ['C', 'D', 'go']]
+
+        m = Stuff.machine
+        m.add_states(states=states, initial='A')
+        m.add_transitions(transitions)
+
+        s = Stuff()
+        with self.assertRaises(error.InvalidTransition):
+            s.dispatch(Event('run'))
+
+        m._switch_state(s, 'B')
+        self.assertEqual(s.state, 'B')
