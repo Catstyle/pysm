8078a501909486ab42865adef8f81889630c329e
haibin <3525004748@qq.com>
2018-03-22 14:47:17 +0800

    added python3 support in setup.py

--- setup.py
+++ setup.py
@@ -64,6 +64,7 @@ setup(
         # In particular, ensure that you indicate whether you support
         # Python 2, Python 3 or both.
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
     ],
     cmdclass={'clean': MyClean}
 )
